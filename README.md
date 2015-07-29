20ab9bbd601a958805bc682d620e77c655414b07
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-29 12:42:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# mongodb-leaf
+
+```
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+  version="1.1" x="0px" y="0px" viewBox="0 0 250 250"
+  enable-background="new 0 0 250 250" xml:space="preserve">
+ <g id="mongodb-leaf">
+  <path id="mongodb-leaf-stem" fill="#A6A385" d="m132.118576,245.311905l-6.359024,-2.172424c0,0 0.776367,-32.416153 -10.856194,-34.742371c-7.755051,-8.99585 1.240746,-381.551559 29.158333,-1.240768c0,0 -9.615448,4.807755 -11.322021,13.02858c-1.861847,8.065567 -0.621094,25.126984 -0.621094,25.126984l0,0l0,0z"/>
+  <path id="mongodb-leaf-right" fill="#499D4A" d="m133.219955,217.975906c0,0 62.153046,-34.939911 43.96405,-116.754906c-12.562012,-55.3708 -42.188004,-73.5177 -45.444,-80.497801c-3.567001,-4.963 -6.980003,-13.64824 -6.980003,-13.64824l2.326996,154.015036c0,0.156006 -6.245499,51.043503 6.008507,56.782501"/>
+  <path id="mongodb-leaf-left" fill="#58AA50" d="m122.50312,215.996826c0,0 -52.269646,-35.674057 -49.167053,-98.489357c2.945869,-62.816769 39.86071,-93.681702 46.994659,-99.265772c4.6539,-4.962999 4.809174,-6.824864 5.119713,-11.787839c3.256439,6.980127 2.63678,104.384009 3.101166,115.86132c1.396027,44.204063 -2.481499,85.306969 -6.048485,93.681648l0,0l0,0z"/>
+ </g>
+</svg>
+```
+
+## .ico
+
+![](dist/mongodb-leaf.ico)
+
+## .icns
+
+[mongodb-leaf.icns](dist/mongodb-leaf.icns)
+
+## .png
+
+### 16x16
+> Favicon, List views, Spotlight Searches
+
+![](dist/mongodb-leaf_16x16.png)
+
+### 32x32
+> Finder
+
+![](dist/mongodb-leaf_32x32.png)
+
+### 128x128
+> Dock and Finder Previews
+
+![](dist/mongodb-leaf_128x128.png)
+
+### 256x256
+> Finder Preview
+
+![](dist/mongodb-leaf_256x256.png)
+
+### 512x512
+> CoverFlow
+
+![](dist/mongodb-leaf_512x512.png)
+
+## .png@2x
+### 16x16
+
+![](dist/mongodb-leaf_16x16@2x.png)
+
+### 32x32
+
+![](dist/mongodb-leaf_32x32@2x.png)
+
+### 128x128
+
+![](dist/mongodb-leaf_128x128@2x.png)
+
+### 256x256
+
+![](dist/mongodb-leaf_256x256@2x.png)
+
+### 512x512
+
+![](dist/mongodb-leaf_512x512@2x.png)
---
+++ gulpfile.js
@@ -0,0 +1,158 @@
+var gulp = require('gulp');
+var svg_to_png = require('svg-to-png');
+var through = require('through2');
+var flatten = require('lodash.flatten');
+var path = require('path');
+var async = require('async');
+var fs = require('fs-extra');
+var del = require('del');
+var jade = require('gulp-jade');
+var webserver = require('gulp-webserver');
+var rename = require('gulp-rename');
+var shell = require('gulp-shell');
+var im = require('imagemagick');
+
+var SIZES = {
+  'Favicon, List views, Spotlight Searches': 16,
+  'Finder': 32,
+  'Dock and Finder Previews': 128,
+  'Finder Preview': 256,
+  'CoverFlow': 512
+};
+
+function createPNGs(SIZES) {
+  return through.obj(function(file, enc, callback) {
+    // Pass file through if:
+    // - file has no contents
+    // - file is a directory
+    if (file.isNull() || file.isDirectory()) {
+      this.push(file);
+      return callback();
+    }
+    // Skip anything that's accidentally not an SVG
+    if (['.svg'].indexOf(path.extname(file.path)) === -1) {
+      this.push(file);
+      return callback();
+    }
+    var self = this;
+    var src = file.path;
+    var basename = path.basename(file.path).split('.')[0];
+
+    var pngTasks = flatten(Object.keys(SIZES).map(function(name) {
+      var s = [];
+      var px = SIZES[name];
+      var dest = './dist/' + basename + '_' + px + 'x' + px + '-tmp';
+      var dest_retina = './dist/' + basename + '_' + px + 'x' + px + '@2x-tmp';
+
+      s.push(function(cb) {
+        console.log('Generating %s %dx%d...', name, px, px);
+        svg_to_png.convert(src, dest, {
+          defaultHeight: px,
+          defaultWidth: px
+        }).then(function() {
+          fs.move(dest + '/' + basename + '.png', dest.replace('-tmp', '.png'), function() {
+            console.log('completed ', name);
+            cb();
+          });
+        });
+      });
+
+      s.push(function(cb) {
+        console.log('Generating %s retina %dx%d...', name, px * 2, px * 2);
+        svg_to_png.convert(src, dest_retina, {
+          defaultHeight: px * 2,
+          defaultWidth: px * 2
+        }).then(function() {
+          fs.move(dest_retina + '/' + basename + '.png', dest_retina.replace('-tmp', '.png'), function() {
+            console.log('completed ', name + ' (Retina)');
+            cb();
+          });
+        });
+      });
+      return s;
+    }));
+
+    console.log('running %d pngTasks...', pngTasks.length);
+    async.series(pngTasks, function() {
+      del('dist/*-tmp', function() {
+        self.push(file);
+        return callback();
+      });
+    });
+  });
+}
+
+gulp.task('png', function() {
+  gulp.src('./mongodb-leaf.svg')
+    .pipe(createPNGs(SIZES))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('html', function() {
+  gulp.src('./index.jade')
+    .pipe(jade({
+      locals: {
+        sizes: SIZES
+      }
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('ico', function(cb) {
+  im.convert(['dist/mongodb-leaf_16x16.png',
+    '-gravity', 'center',
+    '-background', 'transparent',
+    '-crop', '16x16+0+0',
+    '-colors', '256',
+    '-flatten',
+    'dist/mongodb-leaf.ico'
+  ], cb);
+});
+
+gulp.task('iconset:copy', function() {
+  return gulp.src('dist/*.png')
+    .pipe(rename(function(p) {
+      p.basename = p.basename.replace('mongodb-leaf', 'icon');
+    }))
+    .pipe(gulp.dest('dist/mongodb-leaf.iconset'));
+});
+
+gulp.task('iconset', ['iconset:copy'], shell.task('iconutil -c icns dist/mongodb-leaf.iconset'));
+
+gulp.task('webserver', function() {
+  gulp.src('dist/')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+gulp.task('markdown', function() {
+  console.log('## Sizes');
+
+  Object.keys(SIZES).map(function(name) {
+    var px = SIZES[name];
+
+    console.log('### ' + px + 'x' + px);
+    console.log('> ' + name);
+    console.log();
+    console.log('![](dist/mongodb-leaf_%sx%s.png)', px, px);
+    console.log();
+  });
+
+  console.log('## Retina');
+
+  Object.keys(SIZES).map(function(name) {
+    var px = SIZES[name];
+
+    console.log('### ' + px + 'x' + px);
+    console.log();
+    console.log('![](dist/mongodb-leaf_%sx%s@2x.png)', px, px);
+    console.log();
+  });
+});
+
+gulp.task('start', ['html', 'webserver'], function() {
+  gulp.watch('index.jade', ['html']);
+});
